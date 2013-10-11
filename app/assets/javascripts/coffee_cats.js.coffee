ec791a8ddcc56db7f3a6976dfd4174ea40321b1d
Patrick Slattery <slattery.patrickj@gmail.com>
2013-10-11 15:13:29 -0700

    initial commit

---
+++ app/assets/javascripts/coffee_cats.js.coffee
@@ -0,0 +1,9 @@
+window.CoffeeCats =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  initialize: -> alert 'Hello from Backbone!'
+
+$(document).ready ->
+  CoffeeCats.initialize()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeCats::Application.config.secret_token = 'f24c944be4e7193504177c0b25ac78d4bdae2354d7f297725bbb3b885a7a750aafc61f066c93175bd46e6077164359857f2dca85d92ee53b32a8c00ca9954ac3'
